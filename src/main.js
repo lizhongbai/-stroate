e2c80d4773c387e577ac441bab3982254dbaa8a3
bailizhong <564053391@qq.com>
2018-07-09 01:24:16 +0800

    完成增，删，状态，

--- src/main.js
+++ src/main.js
@@ -5,9 +5,15 @@ import ElementUI from 'element-ui'
 import 'element-ui/lib/theme-chalk/index.css'
 import './assets/css/base.css'
 import MyAxios from './plugins/myasiox'
+import moment from 'moment'
 Vue.config.productionTip = false
 Vue.use(ElementUI)
 Vue.use(MyAxios)
+//定义格式时间的插件 ,定义全局过滤器定义时间
+Vue.filter('dataForm',function(dataStr,pattern="YYYY-MM-DD HH:mm:ss"){
+    return moment(dataStr).format(pattern)
+})
+
 
 /* eslint-disable no-new */
 new Vue({
--- src/views/user/user.vue
+++ src/views/user/user.vue
@@ -6,14 +6,37 @@
             <el-breadcrumb-item>用户列表</el-breadcrumb-item>
         </el-breadcrumb>
         <div style="margin: 15px 0;">
-            <el-input placeholder="请输入内容" class="input-with-select">
-                <el-button slot="append" icon="el-icon-search"></el-button>
+            <el-input placeholder=" 请输入内容" class="input-with-select" v-model="search">
+                <el-button slot="append" icon="el-icon-search" ></el-button>
             </el-input>
-            <el-button round>添加</el-button>
+						<!--  添加弹出层 -->
+            <el-button round @click="dialogFormVisible = true">添加</el-button>
+						<el-dialog title="用户注册" :visible.sync="dialogFormVisible" width="700px" >
+							<el-form :model="form">
+								<el-form-item label="* 用户名" :label-width="formLabelWidth">
+									<el-input v-model="form.username" auto-complete="off"></el-input>
+								</el-form-item>
+								<el-form-item label="*密码" :label-width="formLabelWidth">
+									<el-input v-model="form.password" auto-complete="off" type="password"></el-input>
+								</el-form-item>
+								<el-form-item label="邮箱" :label-width="formLabelWidth">
+									<el-input v-model="form.email" auto-complete="off"></el-input>
+								</el-form-item>
+								<el-form-item label="电话" :label-width="formLabelWidth">
+									<el-input v-model="form.mobile" auto-complete="off"></el-input>
+								</el-form-item>
+							</el-form>
+							<div slot="footer" class="dialog-footer">
+								<el-button @click="handleUser_">取 消</el-button>
+								<el-button type="primary" @click="handleUserAdd">确 定</el-button>
+							</div>
+						</el-dialog>
         </div>
         <!-- 表格 -->
         <el-table
+				v-loading="loading"
 				border
+				stripe
         :data="userList"
         style="width: 100%">
 						<el-table-column
@@ -28,12 +51,19 @@
             <el-table-column
                 prop="email"
                 label="邮箱"
-                width="200">
+                width="180">
             </el-table-column>
             <el-table-column
                 prop="mobile"
                 label="电话"
-                width="300">
+                width="180">
+            </el-table-column>
+						<el-table-column
+                label="创建时间"
+                width="100">
+								<template slot-scope="scope">
+										{{ scope.row.create_time | dataForm }}
+								</template>
             </el-table-column>
             <el-table-column
                 prop="mg_state"
@@ -41,6 +71,9 @@
 								width="100">
 								<template slot-scope="scope">
 										<el-switch
+											@change="(val) => {
+													handelSwitch(val, scope.row.id)
+												}"
 											v-model="scope.row.mg_state"
 											active-color="#13ce66"
 											inactive-color="#ff4949"
@@ -54,12 +87,22 @@
 								 <template slot-scope="scope">
 									 <el-row class="user-btn">
 										<el-button type="primary" icon="el-icon-edit" circle size="mini" plain></el-button>
-										<el-button type="danger" icon="el-icon-delete" circle size="mini" plain></el-button>
+										<el-button type="danger" icon="el-icon-delete" circle size="mini" plain @click="handelUserDel(scope.row)"></el-button>
 										<el-button type="success" icon="el-icon-check" circle size="mini" plain></el-button>
 									</el-row>
 								</template>
             </el-table-column>
         </el-table>
+				<!-- 分页 -->
+				<!-- <div class="block">
+						<el-pagination
+							:current-page="currentPage4"
+							:page-sizes="[10, 20, 30, 40]"
+							:page-size="10"
+							layout="total, sizes, prev, pager, next, jumper"
+							:total="50">
+						</el-pagination>
+				</div> -->
     </div>
 </template>
 
@@ -68,14 +111,26 @@ export default {
 	data() {
 		return{
 			userList:[],
-			value2: true,
+			loading: true,
+			//弹出层的显示隐藏
+			dialogFormVisible: false,
+			form: {
+				username: '',
+				password:'',
+				email:'',
+				mobile:''
+			},
+			formLabelWidth: '120px',
+			search:'',
 		}
 	},
 	created() {
 		this.getAllUserList()
 	},
 	methods:{
-		getAllUserList() {
+		getAllUserList() {    //全部
+
+			this.loading = true;
 
 			const token = sessionStorage.getItem('token')
 
@@ -92,13 +147,119 @@ export default {
 					if( status === 200 ) {
 						
 						this.userList = data.data.users
+
+						this.loading = false
 						
 					}
 
 				})
 
+		},
+		handleUserAdd() {   //添加
+	
+			const formDate = this.form
+	
+			if( formDate.name  == '') {
+
+				this.$message('请重新输入')
+
+			}else if(formDate.password  == '') {
+				
+				this.$message('请输入密码')
+
+			}
+
+			this.$http.post('users',this.form)
+
+					.then(res =>{
+						
+						const data = res.data
+
+						const { meta:{ meta,status } } = data
+
+						console.log(data)
+
+					if(status === 201) {
+						
+						this.$notify({
+							title: '成功',
+							message: '这是一条成功的提示消息',
+							type: 'success'
+						});
+
+						this.getAllUserList()
+
+						this.username = this.password = this.mobile = this.email = ''
+
+						this.dialogFormVisible = false
+
+					}else{
+
+						this.$message(res.data.meta.msg)
+
+					}
+
+					})
+
+		},
+		handleUser_() { //取消按键
+
+			this.$message('真的不注册了吗？')
+
+		},
+		// 删除
+		handelUserDel(id) {
+			
+			const Myid = id.id
+
+			this.$confirm('确认删除吗?',{
+
+				type: 'warning'
+
+			}).then( async() =>{
+
+				const data = await this.$http.delete('users/' + Myid )
+				
+				if(data.data.meta.status === 200) {
+
+						this.$message('删除成功')
+
+						this.getAllUserList()
+					}
+
+			})
+			
+		},
+		async handelSwitch(val,id) {
+
+			const res = await this.$http.put(`users/${id}/state/${val}`)
+
+			const data = res.data
+
+			const { meta: { meta,status} } = data
+
+			if( status === 200) {
+
+				this.$notify({
+          title: '成功',
+          message: '这是一条成功的提示消息',
+          type: 'success'
+				});
+				
+				this.getAllUserList()
+
+			}else{
+
+				this.$notify({
+          title: '失败',
+          message: '这是一条失败的提示消息',
+          type: 'warning'
+				});
+
+			}
+
 		}
-	}
+	},
 }
 </script>
 
@@ -123,5 +284,8 @@ export default {
 }
 .user-swith {
 	padding-left:20px;
+}
+.block{
+	padding-top: 5px;
 }
 </style>
